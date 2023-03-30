9c490efcc3569415e0c292f85054410705613155
Aryan Garg <gargaryan82000@gmail.com>
2023-03-30 09:43:36 +0530

    Added Seach Page

--- pages/search.js
+++ pages/search.js
@@ -10,8 +10,8 @@ import Map from '../components/Map';
 const Search = () => {
     const router = useRouter();
     const { location, startDate, endDate, noOfGuests } = router.query;
-    const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
-    const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+    const formattedStartDate = startDate && format(new Date(startDate), "dd MMMM yy");
+    const formattedEndDate = endDate && format(new Date(endDate), "dd MMMM yy");
     const range = `${formattedStartDate} - ${formattedEndDate}`;
 
     return (
